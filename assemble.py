5dc3fb86ad98d9a8b7a21177b6472b0ccbe38cb9
trevornyman <trevornyman@gmail.com>
2017-02-22 10:35:22 -0600

    Update assemble.py

--- assemble.py
+++ assemble.py
@@ -1,1 +1,20 @@
 
+HEADERS = {"data" : ["Creation_date",
+                    "Status",
+                    "Completion_date",
+                    "Service_request_number",
+                    "Type_service_request",
+                    "If_yes_where_is_debris_located?",
+                    "Current_activity",
+                    "Most_recent_action",
+                    "Street_address",
+                    "Zip_code",
+                    "X_coordinate",
+                    "Y_coordinate",
+                    "Ward",
+                    "Police_district",
+                    "Community_area",
+                    "Lat",
+                    "Lon",
+                    "Location"]
+          }
