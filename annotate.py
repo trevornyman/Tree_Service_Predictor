cad067b7b5537ac84801fb3c46cd72e2a6a7f217
trevornyman <trevornyman@gmail.com>
2017-02-23 07:37:09 -0600

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -1,1 +1,7 @@
+#split data into testing and training set
+import numpy as np
+from sklearn.cross_validation import train_test_split
 
+X, y = tree_cleaning_data.drop('Wait_time', axis=1), tree_cleaning_data['Wait_time']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
