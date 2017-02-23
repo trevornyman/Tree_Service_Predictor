8a9c2acecef14f433bd8355fce04b0472c5d0c67
trevornyman <trevornyman@gmail.com>
2017-02-23 15:09:17 -0600

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -1,4 +1,15 @@
 #split data into testing and training set
+import os
+import settings
+import pandas as pd
+
+def read():
+  tree_cleaning_data = pd.read_csv("311_Service_Requests_-_Tree_Debris.csv")
+  return tree_cleaning_data
+
+
+
+
 import numpy as np
 from sklearn.cross_validation import train_test_split
 
