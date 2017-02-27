5926f222f237ccda14efed4c4ea6a3fd0c440826
trevornyman <trevornyman@gmail.com>
2017-02-27 14:33:20 -0600

    Update annotate.py

--- annotate.py
+++ annotate.py
@@ -8,7 +8,30 @@ def read():
   return tree_cleaning_data
 
 
+import os
+import settings
+import pandas as pd
 
+
+HEADERS = {"data" : ["Creation_date",
+                    "Status",
+                    "Completion_date",
+                    "Service_request_number",
+                    "Type_service_request",
+                    "If_yes_where_is_debris_located?",
+                    "Current_activity",
+                    "Most_recent_action",
+                    "Street_address",
+                    "Zip_code",
+                    "X_coordinate",
+                    "Y_coordinate",
+                    "Ward",
+                    "Police_district",
+                    "Community_area",
+                    "Lat",
+                    "Lon",
+                    "Location"]
+          }
 
 import numpy as np
 from sklearn.cross_validation import train_test_split
